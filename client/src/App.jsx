0412ec8e24edc577b21424979d0e2b6623c126be
Filousik <Filouseksbox@gmail.com>
2026-03-05 11:47:29 +0000

    5 Mars

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,22 @@
 import './App.css'
-import Header from './components/Header/Header.jsx'
-import Footer from './components/Footer/Footer.jsx'
 import Hero from './components/Hero/Hero'
 import Card from './components/Card/Card.jsx'
+import React, { useState } from "react"
 
 
 
 function App() {
+  const [page, setPage] = useState(0);
+
+  function handleClick(){
+    setPage(page + 1);
+  }
 
   return (
     <>
       
       <Header></Header>
-      <Hero></Hero>
+      <Empty></Empty>
       <Footer></Footer>
       
     </>
@@ -20,7 +24,53 @@ function App() {
 }
 
 export default App
+function Button({page, onClick}){
+  return(
+    <button onClick={onClick}class="btn">Next page</button>
+  )
+}
 
 
+
+
+function Header(){
+  return(
+    <header>
+      
+
+      <nav className='container'>
+          <img src="" alt="" className='logo' />
+          <a class="btn"href="">Home</a>
+          <a class="btn"href="">About</a>
+          <a class="btn"href="">Register</a>
+          <a class="btn"href="">Login</a>
+          
+          
+
+     </nav>
+
+    </header>
+
+)}
+
+
+function Footer(){
+    return(
+        <footer>
+         <h2>Footer</h2>
+        </footer>
+
+
+
+    )
+}
+
+
+function Empty(){
+
+  return(
+    <div class="empty"></div>
+  )
+}
 
 
--- server/server.js
+++ server/server.js
@@ -106,3 +106,5 @@ const cars = [
     condition: "Used"
   }
 ];
+
+
